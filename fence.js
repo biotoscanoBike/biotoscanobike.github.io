56d8ab4a2c8f3830bf8a7fd9e534a84ceb357983
Dr Carlos Toscano <biotoscano@gmail.com>
2025-02-24 23:40:17 +0100

    Actualizar fence.js

--- fence.js
+++ fence.js
@@ -1,3 +1,5 @@
+/*
+
 class Fence {
     constructor(canvas, ctx, lane, entranceWidth, entranceOffset) {
         this.canvas = canvas;
@@ -65,5 +67,50 @@ class Fence {
         return false;
     }
     
+}
+
+*/
+
+class Fence {
+    constructor(z_fence, entranceWidth_world, lane) {
+        this.z_fence = z_fence;
+        this.entranceWidth_world = entranceWidth_world;
+        this.lane = lane;
+    }
+
+    draw(ctx, z_offset, z_near, f, canvas) {
+        const z_relative = this.z_fence - z_offset;
+        if (z_relative < z_near) return;
+        const y_screen = (z_near * canvas.height) / z_relative;
+        if (y_screen > 0 && y_screen < canvas.height) {
+            const x_center_world = this.lane.amplitude * Math.sin(this.lane.frequency * z_relative);
+            const x_left_entrance = x_center_world - this.entranceWidth_world / 2;
+            const x_right_entrance = x_center_world + this.entranceWidth_world / 2;
+            const x_left_screen = canvas.width / 2 + f * x_left_entrance / z_relative;
+            const x_right_screen = canvas.width / 2 + f * x_right_entrance / z_relative;
+            ctx.beginPath();
+            ctx.moveTo(0, y_screen);
+            ctx.lineTo(x_left_screen, y_screen);
+            ctx.moveTo(x_right_screen, y_screen);
+            ctx.lineTo(canvas.width, y_screen);
+            ctx.strokeStyle = 'brown';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+        }
+    }
+
+    checkCollision(bicycle, z_offset, z_near, canvas) {
+        const z_relative = this.z_fence - z_offset;
+        const y_screen = (z_near * canvas.height) / z_relative;
+        if (Math.abs(y_screen - bicycle.y_bicycle) < 10) {
+            const x_center_world = this.lane.amplitude * Math.sin(this.lane.frequency * z_relative);
+            const x_left_entrance = x_center_world - this.entranceWidth_world / 2;
+            const x_right_entrance = x_center_world + this.entranceWidth_world / 2;
+            if (bicycle.x_bicycle_world < x_left_entrance || bicycle.x_bicycle_world > x_right_entrance) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
