f3644453897149609dd82f49fb0f1de0ae90a0b9
Dr Carlos Toscano <biotoscano@gmail.com>
2025-02-24 23:36:31 +0100

    Actualizar game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 
-//*
+/*
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -192,4 +192,4 @@ document.getElementById('startButton').addEventListener('click', () => {
 
 showLandingModal();
 
-*//
+*/
